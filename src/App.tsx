d716368ba6f8274c0f6f68d0d711b2643782f084
Vick-17 <vega.victor.0712@gmail.com>
2024-12-10 12:02:03 +0100

    FEAT: Integration du systeme de route avec react-router-doom

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import Accueil from "./Page/Accueil";
+import Stop from "./Page/Stop";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 function App() {
   return (
-    <div>
-      <Accueil />
-    </div>
+    <Routes>
+      <Route path="/" element={<Accueil />} />
+      <Route path="/stop" element={<Stop />} />
+    </Routes>
   );
 }
 
---
+++ src/Page/Stop.tsx
@@ -0,0 +1,11 @@
+import Header from "../Components/Header/Header";
+const Stop = () => {
+    return (
+      <>
+        <Header />
+        <div></div>
+      </>
+    );
+};
+
+export default Stop;
